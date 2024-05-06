f03a882bb0f8d90f1d30a6698ba9c4d2378990e1
Andrei Zancan <andreizancan@gmail.com>
2024-05-05 22:58:03 -0300

    projeto concluído!!

--- obj/Debug/net8.0/CalculadoraDeDatas.AssemblyInfo.cs
+++ obj/Debug/net8.0/CalculadoraDeDatas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculadoraDeDatas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c05a4b2fb841fca739b2af392c7463b1a13b17e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculadoraDeDatas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculadoraDeDatas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
